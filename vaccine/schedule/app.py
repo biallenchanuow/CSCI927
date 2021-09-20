d2ad88b08c32c1027774b24bfaa5154ab7c69207
Sheng Liu <sl438@uowmail.edu.au>
2021-09-20 17:26:04 +1000

    create schedule api

---
+++ vaccine/schedule/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from route import schedule_blueprint
+from model import db, Schedule, init_app
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ugKWzXj1YFtioYsh-wJ9hw'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/schedule.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_blueprint(schedule_blueprint)
+init_app(app)
+
+migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5003)
---
+++ vaccine/schedule/route.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.wrappers import response
+from model import Schedule, db
+
+
+schedule_blueprint = Blueprint(
+    'schedual_api_routes', __name__, url_prefix='/api/schedule')
+
+
+@schedule_blueprint.route('/all', methods=['GET'])
+def get_all_schedule():
+    all_schedule = Schedule.query.all()
+    result = [schedule.serialize() for schedule in all_schedule]
+    response = {'result': result}
+    return jsonify(result)
+
+
+@schedule_blueprint.route('/create', methods=['POST'])
+def create_schedule():
+    try:
+        schedule = Schedule()
+        schedule.name = request.form['name']
+        schedule.state = request.form['state']
+        schedule.city = request.form['city']
+        schedule.vaccination_cite = request.form['vaccination_cite']
+        schedule.first_slot = request.form['first_slot']
+        schedule.second_slot = request.form['second_slot']
+        schedule.medical_condition = request.form['medical_condition']
+
+        db.session.add(schedule)
+        db.session.commit()
+
+        response = {'message': 'Schedule created',
+                    'result': schedule.serialize()}
+
+    except Exception as e:
+        print(str(e))
+        response = {'message': 'Scheduled failed'}
+
+    return jsonify(response)
