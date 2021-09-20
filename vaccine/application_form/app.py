fa89fda28406db8e65b8d7e0c525ae836e3b5a4b
Sheng Liu <sl438@uowmail.edu.au>
2021-09-20 16:56:00 +1000

    add application form api

---
+++ vaccine/application_form/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from routes import application_blueprint
+from models import db, Application, init_app
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'QzkgdKLNcM1sHZ1pyoJyOA'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/application.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_blueprint(application_blueprint)
+init_app(app)
+
+migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
---
+++ vaccine/application_form/routes.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.wrappers import response
+from models import Application, db
+
+
+application_blueprint = Blueprint(
+    'application_api_routes', __name__, url_prefix='/api/application')
+
+
+@application_blueprint.route('/all', methods=['GET'])
+def get_all_applications():
+    all_applications = Application.query.all()
+    result = [application.serialize() for application in all_applications]
+    response = {'result': result}
+    return jsonify(result)
+
+
+@application_blueprint.route('/create', methods=['POST'])
+def create_application():
+    try:
+        application = Application()
+        application.given_name = request.form['given_name']
+        application.family_name = request.form['family_name']
+        application.gender = request.form['gender']
+        application.indigenous_status = request.form['indigenous_status']
+        application.age = request.form['age']
+        application.place_of_birth = request.form['place_of_birth']
+        application.residential_address = request.form['residential_address']
+        application.residence_status = request.form['residence_status']
+        application.medicare = request.form['medicare']
+        application.vaccine_history = request.form['vaccine_history']
+        application.work_type = request.form['work_type']
+        application.booker_description = request.form['booker_description']
+        application.parking_required = request.form['parking_required']
+        application.interpreter_required = request.form['interpreter_required']
+
+        db.session.add(application)
+        db.session.commit()
+
+        response = {'message': 'Application form created',
+                    'result': application.serialize()}
+
+    except Exception as e:
+        print(str(e))
+        response = {'message': 'Application create failed'}
+
+    return jsonify(response)
