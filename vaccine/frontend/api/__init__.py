b9e64ff1f6c247dd2d1c9682f4a7bf3d7bbe06dc
Sheng Liu <sl438@uowmail.edu.au>
2021-09-25 15:55:11 +1000

    update

--- vaccine/frontend/api/__init__.py
+++ vaccine/frontend/api/__init__.py
@@ -1,3 +1,3 @@
-User_API_URL = 'http://user:5001/'
-Application_API_URL = 'http://application:5002/'
-Schedule_API_URL = 'http://schedule:5003/'
+User_API_URL = 'http://user-server:5001/'
+Application_API_URL = 'http://application-server:5002/'
+Schedule_API_URL = 'http://schedule-server:5003/'
